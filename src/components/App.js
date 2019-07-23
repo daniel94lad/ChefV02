b0d1b3013976c4d4946fd9190038940cce7c5034
Daniel Lopez <daniel94lad@gmail.com>
2019-07-22 19:19:59 -0500

    Preparados

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {ReactFragment} from 'react'
+import '../css/App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>LALAL</h1>
+    </div>
+  );
+}
+
+export default App;
